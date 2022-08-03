5f81673104e352996948191c618222e9e766189d
abduuuuuu <abdulmenanmohammed28@gmail.com>
2022-08-03 18:42:17 +0300

    making the web responsive

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
+// script to handle the sticky header
 window.addEventListener("scroll",function(){
     const header = document.querySelector("header");
-    const social = document.getElementsByClassName("social")[0];
     header.classList.toggle("sticky",window.scrollY > 0);
-   
+    nav_bar.classList.remove("nav-active");
+    menu.classList.remove("bx-x");
 })
+
+const menu = document.querySelector(".menu i");
+const nav_bar = document.getElementsByClassName("nav-bar")[0];
+menu.addEventListener("click",function(){
+    nav_bar.classList.toggle("nav-active");
+    menu.classList.toggle("bx-x");
+    menu.classList.toggle("menu-anime");
+})
+
